8902065f2e9d9173c663ea4686332633bb7cd155
ali_rizvi <alirizvi9721@gmail.com>
2025-05-12 13:31:58 +0530

    feat: Implement Command/Query base structure

---
+++ Empo.BuildingBlocks.Application/Configuration/Commands/InternalCommandBase.cs
@@ -0,0 +1,27 @@
+﻿using Empo.BuildingBlocks.Application.Contracts;
+
+namespace Empo.BuildingBlocks.Application.Configuration.Commands;
+
+public abstract class InternalCommandBase : ICommand
+{
+    public Guid Id { get; }
+
+    protected InternalCommandBase(Guid id)
+    {
+        this.Id = id;
+    }
+}
+
+public abstract class InternalCommandBase<TResult> : ICommand<TResult>
+{
+    public Guid Id { get; }
+    protected InternalCommandBase()
+    {
+        this.Id = Guid.NewGuid();
+    }
+
+    protected InternalCommandBase(Guid id)
+    {
+        Id = id;
+    }
+}
---
+++ Empo.BuildingBlocks.Infrastructure/InternalCommand/InternalCommandMaper.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Empo.BuildingBlocks.Infrastructure.InternalCommand;
+
+public class InternalCommandMaper : IInternalCommandMapper
+{
+    private readonly BiDictionary<string, Type> _internalCommandMap;
+
+    public InternalCommandMaper(BiDictionary<string, Type> internalCommandMap)
+    {
+            _internalCommandMap = internalCommandMap;
+    }
+    public string GetName(Type type)
+    {
+        return _internalCommandMap.TryGetSecond(type, out var name) ? name : null;
+    }
+
+    public Type GetType(string name)
+    {
+        return _internalCommandMap.TryGetFirst(name, out var type) ? type : null;
+    }
+}
