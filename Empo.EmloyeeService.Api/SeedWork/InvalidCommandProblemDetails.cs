236b4350be38227ecaade087004cbbae8a79262c
ali_rizvi <alirizvi9721@gmail.com>
2025-05-13 13:01:55 +0530

    feat: Implement Command exception

---
+++ Empo.EmloyeeService.Api/SeedWork/InvalidCommandProblemDetails.cs
@@ -0,0 +1,14 @@
+﻿using Empo.BuildingBlocks.Application;
+
+namespace Empo.EmloyeeService.Api.SeedWork;
+
+public class InvalidCommandProblemDetails : Microsoft.AspNetCore.Mvc.ProblemDetails
+{
+    public InvalidCommandProblemDetails(InvalidCommandException exception)
+    {
+        this.Title = exception.Message;
+        this.Status = StatusCodes.Status400BadRequest;
+        this.Detail = exception.Details;
+        this.Type = "https:/somedomain/validation-error";
+    }
+}
